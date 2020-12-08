2fc69aa97adf6d398eefa9d7339d0805c49cbc99
Damain Ramsajan <dpramsajan@gmail.com>
2020-12-08 17:10:44 +0100

    final backend v1 commit

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 // Importing
-import express from 'express'
-import mongoose from 'mongoose'
-import Messages from './dbMessages.js'
+import express from 'express';
+import mongoose from 'mongoose';
+import Messages from './dbMessages.js';
+import Pusher from "pusher";
+import cors from 'cors';
 
 //App Config
 const app = express();
 const port = process.env.PORT || 8000;
 
+const pusher = new Pusher({
+    appId: "1119521",
+    key: "522980b97f696c0b67e5",
+    secret: "284f5d1d53c75545056f",
+    cluster: "eu",
+    useTLS: true,
+  });
+
 // Middleware
 app.use(express.json());
+app.use(cors());
+
 
 // Db config
 const connect_url= 'mongodb+srv://admin:UIApWECU0y9xdNe7@cluster0.xairj.mongodb.net/whatsappdb?retryWrites=true&w=majority';
@@ -19,6 +31,35 @@ mongoose.connect(connect_url, {
 })
 
 //??
+
+const db = mongoose.connection;
+
+db.once ('open', () => {
+    console.log("db connected");
+
+    const msgCollection = db.collection ('messagecontents');
+    const changeStream = msgCollection.watch();
+
+    changeStream.on ('change', (change) => {
+        console.log('a change happened up in this mfer', change);
+
+        if (change.operationType === 'insert') {
+            const messageDetails = change.fullDocument;
+            pusher.trigger ('messages', 'inserted', 
+            {   name: messageDetails.name,
+                message: messageDetails.message,  
+                timestamp: messageDetails.timestamp,
+                received: messageDetails.received,              
+            },
+           
+            );
+        }else{
+            console.log('error triggering pusher')
+        }
+
+    })
+
+})
 
 // api routes
 app.get("/", (req,res) => res.status(200).send('hello damian'))
